1b0ebd661cf2d1ced9d20df3714d256a1ef4876f
Bdennis88 <70287873+Bdennis88@users.noreply.github.com>
2020-12-15 20:50:32 -0600

    Commit 1

---
+++ .github/CS_ASP_18/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlackOut1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (calendar1.SelectedDate.HasValue && calendar2.SelectedDate.HasValue)
+            {
+                DateTime x = calendar1.SelectedDate.Value;
+                DateTime y = calendar2.SelectedDate.Value;
+                label.Content = (y - x).Days;
+                calendar3.BlackoutDates.Clear();
+                calendar3.BlackoutDates.Add(new CalendarDateRange(x, y));
+            }
+        }
+        private void calendar1_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            l1.Content = calendar1.SelectedDate.Value;
+        }
+        private void calendar2_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            l2.Content = calendar2.SelectedDate.Value;
+        }
+    }
+}
